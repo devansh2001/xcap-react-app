3885b63c8ec99a20d5da92f27d39e6f711e5dfb4
devansh2001 <dponda3@gatech.edu>
2020-08-21 02:03:50 -0400

    window.myKeySet fixed

--- src/App.js
+++ src/App.js
@@ -53,8 +53,11 @@ class App extends Component {
 
     // var raw = JSON.stringify({"body":{"test":["one","two"]}});
 
-    const participant_id = window.myKeySet['PARTICIPANT_ID']
-    window.myKeySet.delete('PARTICIPANT_ID');
+    const participant_id = typeof(window.myKeySet) != 'undefined' ? window.myKeySet['PARTICIPANT_ID'] : null;
+    if (typeof(window.myKeySet) != 'undefined') {
+      window.myKeySet.delete('PARTICIPANT_ID');
+    }
+    
 
     var requestOptions = {
       method: 'POST',
@@ -62,8 +65,8 @@ class App extends Component {
       body: window.myKeySet,
     };
 
-    const url = 'https://xcap-backend-stg.herokuapp.com';
-    // const url = 'http://localhost:5000';
+    // const url = 'https://xcap-backend-stg.herokuapp.com';
+    const url = 'http://localhost:5000';
 
     await fetch(url + '/get-questions', requestOptions)
     .then(response => response.json())
